92d5dae1e5c1cc6282c1fba4c7bc5cd03b8345da
Roberto Rodriguez <roberto@coalicionsur.org>
2018-05-13 14:56:53 -0600

    fc

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const querystring = require('querystring');
 var url = require("url");
 var path = require("path");
+var JWT = require("@positiveion/proton/helpers/jwt");
 
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
@@ -15,6 +16,10 @@ function App(event) => {
   const request = event.Records[0].cf.request;
   console.log(JSON.stringify(event));
   console.log(request);
+  var authorization = request.headers.authorization || request.headers.authorization;
+  console.log(authorization);
+
+
   request.uri = request.uri.replace("api/", "");
   return request;
 
