646ea04398982546bd197506abc23449fc7f11a4
Roberto Rodriguez <roberto@coalicionsur.org>
2018-05-13 15:14:10 -0600

    no proton

--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 var chai = require('chai');
 var should = chai.should();
-var BbPromise = require("bluebird");
-var AppTester = require("@positiveion/proton/service/appTester");
 
 
 //Our parent block
